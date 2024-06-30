e3b27a90681ef13ff9054496f713ad72489ba448
eniverz <mspt5871@protonmail.com>
2024-06-30 21:15:52 +0800

    add dotfiles

---
+++ config/i3/mon.sh
@@ -0,0 +1,3 @@
+xrandr --setprovideroutputsource modesetting NVIDIA-0
+xrandr --output HDMI-0 --left-of eDP-1-1 --auto
+xrandr --output eDP-1-1 --scale 0.55x0.55 --auto
---
+++ config/rofi/powermenu.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rofi -show p -modi p:~/.config/rofi/off.sh -no-default-config -config=~/.config/rofi/config.rasi
