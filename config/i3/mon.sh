ff4245002552cb80614cd7ce972601caf96f0670
eniverz <mspt5871@protonmail.com>
2024-07-01 19:00:01 +0800

    multi monitor

--- config/i3/mon.sh
+++ config/i3/mon.sh
@@ -1,3 +1,7 @@
+sleep 2s
+
 xrandr --setprovideroutputsource modesetting NVIDIA-0
 xrandr --output HDMI-0 --left-of eDP-1-1 --auto
 xrandr --output eDP-1-1 --scale 0.55x0.55 --auto
+
+feh --bg-fill ~/Pictures/崩坏/我要把所有叫我希尔的人全鲨了/IMG_1720.PNG
