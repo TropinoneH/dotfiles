de2a5271b7ed2050f9fc462a22d3b7c3cd84da0b
TropinoneH <mspt5871@protonmail.com>
2024-07-04 02:19:56 +0800

    make yay more automatic

--- yay_install.sh
+++ yay_install.sh
@@ -22,7 +22,7 @@ cd .. || exit
 
 git clone https://aur.archlinux.org/fcitx-sogoupinyin.git
 cd fcitx-sogoupinyin || exit
-# cp ~/Downloads/pkgs/office/sogoupinyin_4.2.1.145_amd64.deb ./
+curl -O https://ime-sec.gtimg.com/202407031906/962cda7d3d5a49ce5910c83f5c326ea2/pc/dl/gzindex/1680521603/sogoupinyin_4.2.1.145_amd64.deb
 makepkg -si
 sudo pacman -S fcitx-configtool
 echo "GTK_IM_MODULE=fcitx" | sudo tee -a /etc/environment
@@ -34,16 +34,17 @@ cd .. || exit
 
 git clone https://aur.archlinux.org/linuxqq.git
 cd linuxqq || exit
-sed -i "s/pkgver=[0-9]\.[0-9]\.[0-9]*_[0-9]\{5\}/pkgver=3.2.2_18394/g" PKGBUILD
-sed -i "s/_pkgver_loong64=[0-9]\.[0-9]\.[0-9]*_[0-9]\{5\}/_pkgver_loong64=3.2.2_18394/g" PKGBUILD
+git checkout 0ba2ff0
 makepkg -si
 cd .. || exit
 
 git clone https://aur.archlinux.org/typora.git
 cd typora || exit
-sed -i "s/pkgver=[0-9]\.[0-9]\.[0-9]/1.6.6/g"
+sed -i "s/pkgver=[0-9]\.[0-9]\.[0-9]/pkgver=1.6.6/g" PKGBUILD
+sed -i "s/sha512sums=('[0-9a-z]*'/sha512sums=('SKIP'/" PKGBUILD
 makepkg -si
 cd ~/Documents/go || exit
+rm -rf ElectronInjector
 git clone git@github.com:Yoshino-s/ElectronInjector.git
 cd ElectronInjector || exit
 go get
