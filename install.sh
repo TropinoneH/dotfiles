94d297ff1d13996fc1154b4e7a4e915d422e8570
TropinoneH <mspt5871@protonmail.com>
2024-07-04 02:19:16 +0800

    force ln

--- install.sh
+++ install.sh
@@ -3,18 +3,21 @@
 cwd=$HOME/dotfiles
 echo $cwd
 for file in $cwd/config/*; do
-    ln -s "$file" $HOME/.config/$(basename $file)
+    ln -sf "$file" $HOME/.config/$(basename $file)
 done
 
-ln -s $cwd/zsh $HOME/.zsh
-ln -s $cwd/zshrc $HOME/.zshrc
-ln -s $cwd/xprofile $HOME/.xprofile
-ln -s $cwd/p10k.zsh $HOME/.p10k.zsh
+ln -sf $cwd/zsh $HOME/.zsh
+ln -sf $cwd/zshrc $HOME/.zshrc
+ln -sf $cwd/xprofile $HOME/.xprofile
+ln -sf $cwd/p10k.zsh $HOME/.p10k.zsh
 
 for file in $cwd/desktop/*; do
-    sudo ln -s $file /usr/share/applications/$(basename $file)
+    sudo ln -sf $file /usr/share/applications/$(basename $file)
 done
 
-sudo ln -s $cwd/eduroam.8021x /var/lib/iwd/eduroam.8021x
+sudo ln -sf $cwd/eduroam.8021x /var/lib/iwd/eduroam.8021x
 
 sudo cp $cwd/fonts/* /usr/share/fonts/
+sudo cp $cwd/nord-theme /usr/share/themes/
+
+ln -sf $cwd/Xresources ~/.Xresources
