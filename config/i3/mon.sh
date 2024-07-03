9dd4def7e5830716217e549d3f10ff39348d2983
eniverz <mspt5871@protonmail.com>
2024-07-04 02:56:51 +0800

    use low resolution instead of scale

--- config/i3/mon.sh
+++ config/i3/mon.sh
@@ -1,7 +1,7 @@
-sleep 5s
+sleep 2s
 
 xrandr --setprovideroutputsource modesetting NVIDIA-0
-xrandr --output HDMI-0 --left-of eDP-1-1 --auto
-xrandr --output eDP-1-1 --scale 0.55x0.55 --auto
+xrandr --output HDMI-0 --left-of eDP-1-1 --auto --rate 75
+xrandr --output eDP-1-1 --mode 1920x1200
 
 feh --bg-fill ~/Pictures/崩坏/我要把所有叫我希尔的人全鲨了/IMG_1720.PNG
