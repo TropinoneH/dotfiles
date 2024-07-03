3546950f69f3686c85147f8c1dbe5395ffd52af4
TropinoneH <mspt5871@protonmail.com>
2024-07-04 02:57:12 +0800

    use gnome-keyring for mailspring key-manager

--- pacman_install.sh
+++ pacman_install.sh
@@ -3,7 +3,7 @@
 # firmware
 sudo pacman -S nvidia mesa pulseaudio-bluetooth bluedevil alsa-utils sof-firmware plasma-framework5 libmpdclient wezterm bbswitch acpi
 
-sudo pacman -S lolcat fd fzf ripgrep tree unzip lazygit cmatrix btop neofetch exa ranger dolphin dunst xautolock flameshot arandr lxappearance xclip iwd net-tools tldr wget neovide rofi npm firefox
+sudo pacman -S lolcat fd fzf ripgrep tree unzip lazygit cmatrix btop neofetch exa ranger dolphin dunst xautolock flameshot arandr lxappearance xclip iwd net-tools tldr wget neovide rofi npm firefox gnome-keyring
 
 # init tldr
 git clone git@github.com:tldr-pages/tldr ~/.cache/tldr
