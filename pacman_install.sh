fcf07f754471c0950707631374219284532c45ec
eniverz <mspt5871@protonmail.com>
2024-07-11 00:54:53 +0800

    dependencies for ntpdate

--- pacman_install.sh
+++ pacman_install.sh
@@ -3,7 +3,7 @@
 # firmware
 sudo pacman -S nvidia mesa pulseaudio-bluetooth bluedevil alsa-utils sof-firmware plasma-framework5 libmpdclient wezterm bbswitch acpi
 
-sudo pacman -S lolcat fd fzf ripgrep tree unzip lazygit cmatrix btop neofetch exa ranger dolphin dunst xautolock flameshot arandr lxappearance xclip iwd net-tools tldr wget neovide npm firefox gnome-keyring ncmpcpp mpd mpc
+sudo pacman -S lolcat fd fzf ripgrep tree unzip lazygit cmatrix btop neofetch exa ranger dolphin dunst xautolock flameshot arandr lxappearance xclip iwd net-tools tldr wget neovide npm firefox gnome-keyring ncmpcpp mpd mpc libevent gnome-keyring
 
 # init tldr
 git clone git@github.com:tldr-pages/tldr ~/.cache/tldr
