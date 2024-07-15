2d13ad4342a55ceadaf2ff01f843de165fb07a4a
TropinoneH <mspt5871@protonmail.com>
2024-07-16 04:12:26 +0800

    temp

--- config/i3/mon.sh
+++
@@ -1,7 +0,0 @@
-sleep 2s
-
-xrandr --setprovideroutputsource modesetting NVIDIA-0
-xrandr --output HDMI-0 --left-of eDP-1-1 --auto --rate 75
-# xrandr --output eDP-1-1 --mode 1920x1200
-
-feh --bg-fill ~/Pictures/崩坏/我要把所有叫我希尔的人全鲨了/IMG_1720.PNG
--- yay_install.sh
+++ yay_install.sh
@@ -7,11 +7,10 @@ go env -w GO111MODULE=on
 go env -w GOPROXY=https://mirrors.aliyun.com/goproxy/,direct
 export GOPROXY=https://mirrors.aliyun.com/goproxy/,direct
 
-git clone https://aur.archlinux.org/yay.git
-cd yay || exit
-sed -i "s/makedepends=('go>=1.21')//g" PKGBUILD
-makepkg -si
-cd .. || exit
+# git clone https://aur.archlinux.org/yay.git
+# cd yay || exit
+# makepkg -si
+# cd .. || exit
 
 # use exa as alternative
 # git clone https://aur.archlinux.org/logo-ls.git
@@ -20,41 +19,38 @@ cd .. || exit
 # makepkg -si
 # cd .. || exit
 
-git clone https://aur.archlinux.org/fcitx-sogoupinyin.git
-cd fcitx-sogoupinyin || exit
-curl -O https://ime-sec.gtimg.com/202407031906/962cda7d3d5a49ce5910c83f5c326ea2/pc/dl/gzindex/1680521603/sogoupinyin_4.2.1.145_amd64.deb
-makepkg -si
-sudo pacman -S fcitx-configtool
-echo "GTK_IM_MODULE=fcitx" | sudo tee -a /etc/environment
-echo "QT_IM_MODULE=fcitx" | sudo tee -a /etc/environment
-echo "XMODIFIERS=@im=fcitx" | sudo tee -a /etc/environment
-echo "SDL_IM_MODULE=fcitx" | sudo tee -a /etc/environment
-echo "GLFW_IM_MODULE=ibus" | sudo tee -a /etc/environment
-cd .. || exit
+# git clone https://aur.archlinux.org/fcitx-sogoupinyin.git
+# cd fcitx-sogoupinyin || exit
+# curl -O https://ime-sec.gtimg.com/202407031906/962cda7d3d5a49ce5910c83f5c326ea2/pc/dl/gzindex/1680521603/sogoupinyin_4.2.1.145_amd64.deb
+# makepkg -si
+# sudo pacman -S fcitx-configtool
+# echo "GTK_IM_MODULE=fcitx" | sudo tee -a /etc/environment
+# echo "QT_IM_MODULE=fcitx" | sudo tee -a /etc/environment
+# echo "XMODIFIERS=@im=fcitx" | sudo tee -a /etc/environment
+# echo "SDL_IM_MODULE=fcitx" | sudo tee -a /etc/environment
+# echo "GLFW_IM_MODULE=ibus" | sudo tee -a /etc/environment
+# cd .. || exit
 
-git clone https://aur.archlinux.org/linuxqq.git
-cd linuxqq || exit
-git checkout 0ba2ff0
-makepkg -si
-cd .. || exit
+# git clone https://aur.archlinux.org/linuxqq.git
+# cd linuxqq || exit
+# git checkout 0ba2ff0
+# makepkg -si
+# cd .. || exit
 
-git clone https://aur.archlinux.org/typora.git
-cd typora || exit
-sed -i "s/pkgver=[0-9]\.[0-9]\.[0-9]/pkgver=1.6.6/g" PKGBUILD
-sed -i "s/sha512sums=('[0-9a-z]*'/sha512sums=('SKIP'/" PKGBUILD
-makepkg -si
-cd ~/Documents/go || exit
-rm -rf ElectronInjector
-git clone git@github.com:Yoshino-s/ElectronInjector.git
-cd ElectronInjector || exit
-go get
-go build -o main
-cd /usr/share/typora/resources/app.asar.unpacked || exit
-sudo ~/Documents/go/ElectronInjector/main -i crack
-cd ~/Downloads/pkgs/pacman || exit
+# # git clone https://aur.archlinux.org/typora.git
+# cd typora || exit
+# sed -i "s/pkgver=[0-9]\.[0-9]\.[0-9]/pkgver=1.6.6/g" PKGBUILD
+# sed -i "s/sha512sums=('[0-9a-z]*'/sha512sums=('SKIP'/" PKGBUILD
+# makepkg -si
+# cd ~/Documents/go || exit
+# rm -rf ElectronInjector
+# git clone git@github.com:Yoshino-s/ElectronInjector.git
+# cd ElectronInjector || exit
+# go get
+# go build -o main
+# cd /usr/share/typora/resources/app.asar.unpacked || exit
+# sudo ~/Documents/go/ElectronInjector/main -i crack
+# cd ~/Downloads/pkgs/pacman || exit
 
 # yay install
-yay -S google-chrome optimus-manager optimus-manager-qt networkmanager-dmenu-git rofi-bluetooth-git mailspring rofi-greenclip notion-app-electron todesk visual-studio-code-bin clang-format-all-git ueberzugpp cava
-
-sudo pacman -Rs i3lock
-yay -S i3lock-color
+yay -S google-chrome mailspring notion-app-electron visual-studio-code-bin clang-format-all-git ueberzugpp cava
