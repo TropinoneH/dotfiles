59228136c7de5b600b43027a2297b455ce9ce9a7
eniverz <mspt5871@protonmail.com>
2024-07-17 03:33:49 +0800

    update installation

--- pacman_install.sh
+++ pacman_install.sh
@@ -3,7 +3,10 @@
 # firmware
 sudo pacman -S mesa pulseaudio-bluetooth bluedevil alsa-utils sof-firmware plasma-framework5 libmpdclient wezterm bbswitch acpi
 
-sudo pacman -S lolcat fd fzf ripgrep tree unzip lazygit cmatrix btop fastfetch exa ranger dolphin xautolock flameshot arandr lxappearance xclip iwd net-tools tldr wget neovide npm firefox gnome-keyring ncmpcpp mpd mpc libevent gnome-keyring bat okular brightnessctl mako feh
+sudo pacman -S lolcat fd fzf ripgrep tree unzip lazygit cmatrix btop fastfetch exa yazi dolphin xautolock flameshot arandr xclip iwd net-tools tldr wget neovide npm firefox gnome-keyring ncmpcpp mpd mpc libevent gnome-keyring bat okular brightnessctl mako feh go rust pavucontrol-qt
+
+# font
+sudo pacman -S ttf-jetbrains-mono-nerd otf-codenewroman-nerd adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts
 
 # init tldr
 git clone git@github.com:tldr-pages/tldr ~/.cache/tldr
--- yay_install.sh
+++ yay_install.sh
@@ -8,9 +8,9 @@ go env -w GOPROXY=https://mirrors.aliyun.com/goproxy/,direct
 export GOPROXY=https://mirrors.aliyun.com/goproxy/,direct
 
 # git clone https://aur.archlinux.org/yay.git
-# cd yay || exit
-# makepkg -si
-# cd .. || exit
+cd yay || exit
+makepkg -si
+cd .. || exit
 
 # use exa as alternative
 # git clone https://aur.archlinux.org/logo-ls.git
@@ -19,38 +19,39 @@ export GOPROXY=https://mirrors.aliyun.com/goproxy/,direct
 # makepkg -si
 # cd .. || exit
 
-# git clone https://aur.archlinux.org/fcitx-sogoupinyin.git
-# cd fcitx-sogoupinyin || exit
+git clone https://aur.archlinux.org/fcitx-sogoupinyin.git
+cd fcitx-sogoupinyin || exit
 # curl -O https://ime-sec.gtimg.com/202407031906/962cda7d3d5a49ce5910c83f5c326ea2/pc/dl/gzindex/1680521603/sogoupinyin_4.2.1.145_amd64.deb
-# makepkg -si
-# sudo pacman -S fcitx-configtool
-# echo "GTK_IM_MODULE=fcitx" | sudo tee -a /etc/environment
-# echo "QT_IM_MODULE=fcitx" | sudo tee -a /etc/environment
-# echo "XMODIFIERS=@im=fcitx" | sudo tee -a /etc/environment
-# echo "SDL_IM_MODULE=fcitx" | sudo tee -a /etc/environment
-# echo "GLFW_IM_MODULE=ibus" | sudo tee -a /etc/environment
-# cd .. || exit
+makepkg -si
+sudo pacman -S fcitx-configtool
+echo "GTK_IM_MODULE=fcitx" | sudo tee -a /etc/environment
+echo "QT_IM_MODULE=fcitx" | sudo tee -a /etc/environment
+echo "XMODIFIERS=@im=fcitx" | sudo tee -a /etc/environment
+echo "SDL_IM_MODULE=fcitx" | sudo tee -a /etc/environment
+echo "GLFW_IM_MODULE=ibus" | sudo tee -a /etc/environment
+cd .. || exit
 
-# git clone https://aur.archlinux.org/linuxqq.git
-# cd linuxqq || exit
-# git checkout 0ba2ff0
-# makepkg -si
-# cd .. || exit
+git clone https://aur.archlinux.org/linuxqq.git
+cd linuxqq || exit
+git checkout 0ba2ff0
+makepkg -si
+cd .. || exit
 
-# # git clone https://aur.archlinux.org/typora.git
-# cd typora || exit
-# sed -i "s/pkgver=[0-9]\.[0-9]\.[0-9]/pkgver=1.6.6/g" PKGBUILD
-# sed -i "s/sha512sums=('[0-9a-z]*'/sha512sums=('SKIP'/" PKGBUILD
-# makepkg -si
-# cd ~/Documents/go || exit
-# rm -rf ElectronInjector
-# git clone git@github.com:Yoshino-s/ElectronInjector.git
-# cd ElectronInjector || exit
-# go get
-# go build -o main
-# cd /usr/share/typora/resources/app.asar.unpacked || exit
-# sudo ~/Documents/go/ElectronInjector/main -i crack
-# cd ~/Downloads/pkgs/pacman || exit
+git clone https://aur.archlinux.org/typora.git
+cd typora || exit
+sed -i "s/pkgver=[0-9]\.[0-9]\.[0-9]/pkgver=1.6.6/g" PKGBUILD
+sed -i "s/sha512sums=('[0-9a-z]*'/sha512sums=('SKIP'/" PKGBUILD
+makepkg -si
+cd ~/Documents/go || exit
+rm -rf ElectronInjector
+git clone git@github.com:Yoshino-s/ElectronInjector.git
+cd ElectronInjector || exit
+go get
+go build -o main
+cd /usr/share/typora/resources/app.asar.unpacked || exit
+sudo ~/Documents/go/ElectronInjector/main -i crack
+cd ~/Downloads/pkgs/pacman || exit
 
 # yay install
-yay -S google-chrome mailspring notion-app-electron visual-studio-code-bin clang-format-all-git ueberzugpp cava
+yay -S google-chrome mailspring notion-app-electron visual-studio-code-bin clang-format-all-git ueberzugpp cava hyprlock-git
+yay -S rofi-wayland rofi-bluetooth-git networkmanager-dmenu-git rofi-greenclip
