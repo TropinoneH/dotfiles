69a071a4eb36a48b8fd366dbb5b718055be20078
TropinoneH <mspt5871@protonmail.com>
2024-07-17 18:49:16 +0800

    dependencies for screenshot

--- pacman_install.sh
+++ pacman_install.sh
@@ -3,7 +3,7 @@
 # firmware
 sudo pacman -S mesa pulseaudio-bluetooth bluedevil alsa-utils sof-firmware plasma-framework5 libmpdclient wezterm bbswitch acpi
 
-sudo pacman -S lolcat fd fzf ripgrep tree unzip lazygit cmatrix btop fastfetch exa yazi dolphin xautolock flameshot arandr xclip iwd net-tools tldr wget neovide npm firefox gnome-keyring ncmpcpp mpd mpc libevent gnome-keyring bat okular brightnessctl mako feh go rust pavucontrol-qt
+sudo pacman -S lolcat fd fzf ripgrep tree unzip lazygit cmatrix btop fastfetch exa yazi dolphin xautolock grim arandr xclip iwd net-tools tldr wget neovide npm firefox gnome-keyring ncmpcpp mpd mpc libevent gnome-keyring bat okular brightnessctl mako feh go rust pavucontrol-qt wlroots
 
 # font
 sudo pacman -S ttf-jetbrains-mono-nerd otf-codenewroman-nerd adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts
