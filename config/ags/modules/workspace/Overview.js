de833a26237159c07428e3a2ca812b25c4ed4e0a
eniverz <mspt5871@protonmail.com>
2024-10-05 17:32:46 +0800

    ags: simplify code of workspace overview window

--- config/ags/modules/workspace/Overview.js
+++ config/ags/modules/workspace/Overview.js
@@ -1,29 +1,21 @@
 import OverviewRow from "./OverviewRow.js"
 
-const Hyprland = await Service.import("hyprland")
 const overviewTick = Variable(false)
 
 export default () => {
     const clientMap = new Map()
-    return Widget.Revealer({
-        revealChild: true,
-        // hpack to prevent unneeded expansion in overview-tasks-workspace:
-        hpack: "center",
-        transition: "slide_down",
-        transitionDuration: userConfigs.overview.animations.durationLarge,
-        child: Widget.Box({
-            vertical: true,
-            className: "overview-tasks",
-            children: Array.from({ length: userConfigs.overview.numOfRows }, (_, index) =>
-                OverviewRow(
-                    {
-                        startWorkspace: 1 + index * userConfigs.overview.numOfCols,
-                        workspaces: userConfigs.overview.numOfCols,
-                    },
-                    overviewTick,
-                    clientMap,
-                ),
+    return Widget.Box({
+        vertical: true,
+        className: "overview-tasks",
+        children: Array.from({ length: userConfigs.overview.numOfRows }, (_, index) =>
+            OverviewRow(
+                {
+                    startWorkspace: 1 + index * userConfigs.overview.numOfCols,
+                    workspaces: userConfigs.overview.numOfCols,
+                },
+                overviewTick,
+                clientMap,
             ),
-        }),
+        ),
     })
 }
