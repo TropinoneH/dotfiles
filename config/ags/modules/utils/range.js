3ac600b87300ab2b68b696c6aedbf80c9ea82787
TropinoneH <mspt5871@protonmail.com>
2024-10-05 17:32:19 +0800

    ags: add support for multi-monitors

--- config/ags/modules/utils/range.js
+++ config/ags/modules/utils/range.js
@@ -8,7 +8,24 @@ export const forAllMonitors = (fn = print) => {
     const length = monitors.length
     const arr = []
     for (let i = 0; i < length; i++) {
-        arr.push(fn(monitors[i].id))
+        const win = fn(monitors[i].id)
+        arr.push(win)
     }
+    Hyprland.connect("monitor-added", (_, /**@type{string}*/ name) => {
+        const monitor = Hyprland.monitors.find((mon) => mon.name === name)
+        if (monitor === undefined) return
+        const win = fn(monitor.id)
+        if (win !== undefined) {
+            App.addWindow(win)
+            arr.push(win)
+        }
+    })
+    Hyprland.connect("monitor-removed", (_, /**@type{string}*/ name) => {
+        const monitor = Hyprland.monitors.find((mon) => mon.name === name)
+        if (monitor === undefined) return
+        arr.forEach((item) => {
+            if (item.monitor == monitor.id) App.removeWindow(item)
+        })
+    })
     return arr
 }
