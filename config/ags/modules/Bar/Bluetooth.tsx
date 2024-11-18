72842d771b5ef54e6c946fd737c444b486495241
eniverz <mspt5871@protonmail.com>
2024-11-18 14:50:29 +0800

    ags: fix bugs in bluetooth bar

--- config/ags/modules/Bar/Bluetooth.tsx
+++ config/ags/modules/Bar/Bluetooth.tsx
@@ -1,15 +1,25 @@
 import bluetooth from "gi://AstalBluetooth"
 import { DimButton } from "../components/Button"
-import { bind } from "astal"
+import { Label } from "astal/gtk3/widget"
 import { config } from "../../config"
 const Bluetooth = bluetooth.get_default()
 
+const getConnectedDevices = () => {
+    const devices = Bluetooth.get_devices().filter(d => d.connected).length
+    return devices > 0 ? ` ${devices}` : ""
+}
+
 export default () => {
     const theme = config.theme.bar.bluetooth
+
+    const setup = (label: Label) => {
+        label.hook(Bluetooth, "notify", (_) => label.label = getConnectedDevices())
+    }
+
     return <DimButton onClick={config.bar.bluetooth.onClick}>
         <box css={`background: ${theme.bg}; padding: 0 0.5rem; margin-left: 0.3rem; border-radius: 0.5rem;`}>
             <label label={""} css={`color: ${theme.iconColor}`} />
-            <label label={bind(Bluetooth, "devices").as(devices => devices.filter(d => d.connected).length === 0 ? "" : " " + devices.length.toString())} css={`color: ${theme.color}`} />
+            <label setup={setup} label={getConnectedDevices()} css={`color: ${theme.color}`} />
         </box>
     </DimButton>
 }
