98efa680934a7f8272070e3a8e24d458a8adaafc
eniverz <mspt5871@protonmail.com>
2025-02-19 18:56:19 +0800

    ags: change network tooltip in time

--- config/ags/modules/Bar/Network.tsx
+++ config/ags/modules/Bar/Network.tsx
@@ -1,14 +1,14 @@
 import network from "gi://AstalNetwork"
-import { bind } from "astal"
+import { bind, Variable } from "astal"
 import { EventBoxProps } from "astal/gtk3/widget"
 import { DimButton } from "../components/Button"
 import { config } from "../../config"
 const Network = network.get_default()
 
-const primary = (primary: network.Primary) => ["unknown", "wired", "wifi"][primary]
-const internet = (internet: network.Internet) => ["connected", "connecting", "disconnected"][internet]
+const getPrimary = (primary: network.Primary) => ["unknown", "wired", "wifi"][primary]
+const getInternet = (internet: network.Internet) => ["connected", "connecting", "disconnected"][internet]
 // const connectionState = (state: network.State)
-const deviceState = (state: network.DeviceState) =>
+const getDeviceState = (state: network.DeviceState) =>
     [
         "unknown",
         "unmanaged",
@@ -26,20 +26,39 @@ const deviceState = (state: network.DeviceState) =>
     ][Math.floor(state / 10)]
 
 // Network.wired.get_device().get_type_description() => earthnet
-const Wired = (props: EventBoxProps) => (
-    <DimButton
-        {...props}
-        tooltipText={bind(Network, "wired").as((w) => `${w.device.interface}: ${deviceState(w.state)} | ${internet(w.internet)} | ${w.speed} Mbps`)}
-    >
-        <label label="" css={`color: ${config.theme.bar.network.iconColor};`} />
-    </DimButton>
-)
+const Wired = (props: EventBoxProps) => {
+    const tooltip = Variable.derive(
+        [bind(Network.wired.device, "interface"), bind(Network.wired, "state"), bind(Network.wired, "internet"), bind(Network.wired, "speed")],
+        (deviceInterface, state, internet, speed) => `${deviceInterface}: ${getDeviceState(state)} | ${getInternet(internet)} | ${speed} Mbps`
+    )
+    return (
+        <DimButton {...props} tooltipText={bind(tooltip)} onDestroy={() => tooltip?.drop()}>
+            <label
+                label=""
+                css={`
+                    color: ${config.theme.bar.network.iconColor};
+                `}
+            />
+        </DimButton>
+    )
+}
 
-const WIFI = (props: EventBoxProps) => (
-    <DimButton {...props} tooltipText={bind(Network, "wifi").as((w) => `${w.ssid}: ${internet(w.internet)} | ${w.strength}%`)}>
-        <label label="" css={`color: ${config.theme.bar.network.iconColor};`} />
-    </DimButton>
-)
+const WIFI = (props: EventBoxProps) => {
+    const tooltip = Variable.derive(
+        [bind(Network.wifi, "ssid"), bind(Network.wifi, "internet"), bind(Network.wifi, "strength")],
+        (ssid, internet, strength) => `${ssid}: ${getInternet(internet)} | ${strength}%`
+    )
+    return (
+        <DimButton {...props} tooltipText={bind(tooltip)} onDestroy={() => tooltip?.drop()}>
+            <label
+                label=""
+                css={`
+                    color: ${config.theme.bar.network.iconColor};
+                `}
+            />
+        </DimButton>
+    )
+}
 
 export default () => {
     const theme = config.theme.bar.network
@@ -47,7 +66,7 @@ export default () => {
     return (
         <box css="margin-left: 0.3rem;">
             <stack
-                shown={bind(Network, "primary").as((p) => primary(p))}
+                shown={bind(Network, "primary").as((p) => getPrimary(p))}
                 css={`
                     color: ${theme.color};
                     background: ${theme.bg};
@@ -56,9 +75,14 @@ export default () => {
                 `}
             >
                 <DimButton name="unknown" onClick={onClick}>
-                    <label label={"󰖪"} css={`color: ${theme.iconDisconnectColor};`} />
+                    <label
+                        label={"󰖪"}
+                        css={`
+                            color: ${theme.iconDisconnectColor};
+                        `}
+                    />
                 </DimButton>
-                <Wired name="wired" onClick={onClick} />
+                <box name="wired">{bind(Network, "wired").as((w) => (w ? <Wired name="wired" onClick={onClick} /> : <></>))}</box>
                 <WIFI name="wifi" onClick={onClick} />
             </stack>
         </box>
