249626377d7b687518a22365e6743cb73fd4e88f
eniverz <mspt5871@protonmail.com>
2025-04-10 18:37:21 +0800

    yazi: update config for break update

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,17 +1,17 @@
-THEME.git = THEME.git or {}
-THEME.git.modified_sign = ""
-THEME.git.added_sign = ""
-THEME.git.untracked_sign = "󰞋"
-THEME.git.ignored_sign = ""
-THEME.git.deleted_sign = ""
-THEME.git.updated_sign = ""
+th.git = th.git or {}
+th.git.modified_sign = ""
+th.git.added_sign = ""
+th.git.untracked_sign = "󰞋"
+th.git.ignored_sign = ""
+th.git.deleted_sign = ""
+th.git.updated_sign = ""
 
-THEME.git.modified = ui.Style():fg("#f9e2af")
-THEME.git.added = ui.Style():fg("#a6e3a1")
-THEME.git.untracked = ui.Style():fg("#89dceb")
-THEME.git.ignored = ui.Style():fg("#6c7087")
-THEME.git.deleted = ui.Style():fg("#f38ba8")
-THEME.git.updated = ui.Style():fg("#94e2d5")
+th.git.modified = ui.Style():fg("#f9e2af")
+th.git.added = ui.Style():fg("#a6e3a1")
+th.git.untracked = ui.Style():fg("#89dceb")
+th.git.ignored = ui.Style():fg("#6c7087")
+th.git.deleted = ui.Style():fg("#f38ba8")
+th.git.updated = ui.Style():fg("#94e2d5")
 
 require("git"):setup()
 
